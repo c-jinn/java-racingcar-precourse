e77ade5d078c97cd3e9f0fd9bd832b0d0fedbe59
Changjin <quritis@naver.com>
2022-10-09 00:44:06 +0900

    기능요구사항 초안 정리

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -1,2 +1,21 @@
-package racingcar;public class CarTest {
+package racingcar;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CarTest {
+
+
+    private Car car;
+
+    @Test
+    void 전진(){
+        CarCondition carCondition = car.run(5);
+
+        assertThat(carCondition).isEqualTo(CarCondition.Forword);
+
+    }
+
+
 }
